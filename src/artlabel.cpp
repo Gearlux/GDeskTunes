2dff42d779439aecf5d650b944053cd0743f11de
Gearlux <gert.behiels@gmail.com>
2014-12-09 21:59:10 +0100

    Some improvements for the mini player

---
+++ src/artlabel.cpp
@@ -0,0 +1,6 @@
+#include "artlabel.h"
+
+ArtLabel::ArtLabel(QObject *parent) :
+    QLabel(parent)
+{
+}
---
+++ src/artlabel.h
@@ -0,0 +1,18 @@
+#ifndef ARTLABEL_H
+#define ARTLABEL_H
+
+#include <QLabel>
+
+class ArtLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    explicit ArtLabel(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // ARTLABEL_H
