685aeced65b8f8d0f943f2e658b136229c1be261
Gearlux <gert.behiels@gmail.com>
2014-12-09 15:29:35 +0100

    Deployment for windows

---
+++ deploy/windows.bat
@@ -0,0 +1,13 @@
+del /Q GDeskTunes.zip
+rmdir /S /Q GDeskTunes
+mkdir GDeskTunes
+xcopy /S ..\..\..\Builds\build-GDeskTunes-Desktop_Qt_5_4_MSVC2012_OpenGL_32bit-Release\release GDeskTunes
+del GDeskTunes\*.obj
+del GDeskTunes\*.cpp
+del GDeskTunes\*.moc
+windeployQt GDeskTunes\GDeskTunes.exe
+zip -r GDeskTunes.zip GDeskTunes
+
+
+
+
