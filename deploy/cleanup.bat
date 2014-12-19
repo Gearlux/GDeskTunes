803271b33aaba28fb60a42c5b32142dd3879ecc6
Gearlux <gert.behiels@gmail.com>
2014-12-19 13:19:49 +0100

    New deployment

---
+++ deploy/cleanup.bat
@@ -0,0 +1,7 @@
+del /Q GDeskTunes.dmg
+del /Q GDeskTunes.app
+del /Q GDeskTunes_linux.tgz
+del /Q GDeskTunesInstaller.dmg
+del /Q GDeskTunesInstaller.app
+del /Q /S GDeskTunes
+del /Q /S installer\packages\org.gearlux.gdesktunes\data\*
---
+++ deploy/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -f GDeskTunes.dmg
+rm -rf GDeskTunes.app
+rm -f GDeskTunes_linux.tgz
+rm -f GDeskTunesInstaller.dmg
+rm -fr GDeskTunesInstaller.app
+rm -rf GDeskTunes
+rm -rf installer/packages/org.gearlux.gdesktunes/data/*
